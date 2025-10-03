dff1ab18f5ee12579d17c0ddfea9bd57c157084d
Atambek <117630074+Atambek07@users.noreply.github.com>
2025-10-03 23:20:10 +0600

    Update lab_1.7.cpp

--- lab_1.7.cpp
+++ lab_1.7.cpp
@@ -1,22 +1,22 @@
-// 1.7. 
 #include <iostream>
 using namespace std;
 
 int main() {
-	setlocale(LC_ALL, "Russian");   
-    double kg;
-    const double pricePerOunce = 1900.0;
+    setlocale(LC_ALL, "Russian");
+    double grams;
+    const double ounceWeight = 28.3495;   
+    const double pricePerOunce = 3843.03; 
 
-    cout << "Введите вес золота (кг): ";
-    cin >> kg;
+    cout << "Введите вес золота (г): ";
+    cin >> grams;
 
-    double ounces = kg * 31.103;
+    double ounces = grams / ounceWeight;
 
     double totalPrice = ounces * pricePerOunce;
 
-    cout << "\nВес золота в тройских унциях: " << ounces << " унций" << endl;
+    cout << "\nВес золота в унциях: " << ounces << " унций" << endl;
     cout << "Стоимость золота: " << totalPrice << " USD" << endl;
 
-	system("pause");
+    system("pause");
     return 0;
 }
