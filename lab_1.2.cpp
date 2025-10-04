3ae54649cfa453c16bb4f5997ef8aec5f64900c6
Atambek <117630074+Atambek07@users.noreply.github.com>
2025-10-04 23:12:43 +0600

    Update lab_1.2.cpp

--- lab_1.2.cpp
+++ lab_1.2.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Rus");
-	double a, b, c, h, s, p;
-	cout << "Введите a, b, c, h = " << endl;
-	cin >> a >> b >> c >> h;
-	s = ((a + b) / 2) * h;
-	p = a + b + 2 * c;
-	cout << "Площадь = " << s << endl;
-	cout << "Пертметр = " << p << endl;
+	double a, a1, b, h, c, s, p;
+	cout << "Введите большое основание =";
+	cin >> a;
+	cout << "Введите малое основание =";
+	cin >> a1;
+	cout << "Введите высоту = ";
+	cin >> h;
+	c = (a - a1) / 2;
+	b = sqrt(pow(h, 2) + pow((a - a1) / 2, 2));
+	s = (a + a1) / 2 * h;
+	p = a + a1 + b + b;
+	cout << "Площадь =" << s << endl;
+	cout << "Пертметр =" << p << endl;
 	system("pause");
 	return 0;
 }
+
