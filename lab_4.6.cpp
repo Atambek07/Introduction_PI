3ca61985fcb575fc3e59fc8bace0e045893113b9
Atambek <117630074+Atambek07@users.noreply.github.com>
2025-10-31 01:48:57 +0600

    Implement array processing with zero element checks

---
+++ lab_4.6.cpp
@@ -0,0 +1,59 @@
+// Задача 6
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    srand(time(NULL));
+
+    const int N = 26;
+    int a[N] = {}, firstZero, sum, p, rmax, rmin;
+    firstZero = -1, sum = 0, p = 1;
+
+    for (int i = 0; i < N; i++) {
+        a[i] = rand() % 21 - 10;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (a[i] == 0) {
+            firstZero = i;
+            break;
+        }
+        sum += a[i];
+        p *= a[i];
+    }
+
+    if (firstZero != -1 && firstZero < N - 1) {
+        rmax = a[firstZero + 1];
+        rmin = a[firstZero + 1];
+        for (int i = firstZero + 1; i < N; i++) {
+            if (a[i] > rmax) rmax = a[i];
+            if (a[i] < rmin) rmin = a[i];
+        }
+    }
+
+    cout << "Массив: ";
+    for (int i = 0; i < N; i++) cout << a[i] << " ";
+    cout << endl;
+
+    if (firstZero != -1) {
+        cout << "Номер первого нулевого элемента: " << firstZero << endl;
+        cout << "Сумма элементов до нуля: " << sum << endl;
+        cout << "Произведение элементов до нуля: " << p << endl;
+        if (firstZero < N - 1) {
+            cout << "Максимальный элемент после нуля: " << rmax << endl;
+            cout << "Минимальный элемент после нуля: " << rmin << endl;
+        }
+    }
+    else {
+        cout << "В массиве нет нулевых элементов." << endl;
+        cout << "Сумма всех элементов: " << sum << endl;
+        cout << "Произведение всех элементов: " << p << endl;
+    }
+
+    system("pause");
+    return 0;
+}
