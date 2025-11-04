1901e5ea2f6fc5c3af8c441779d7b8f3e41276cf
Atambek <117630074+Atambek07@users.noreply.github.com>
2025-11-04 13:32:15 +0600

    Enhance temperature array output and fix minTemp logic
    Added output for temperature array and adjusted logic for minTemp comparison.

--- lab_4.4.cpp
+++ lab_4.4.cpp
@@ -14,10 +14,15 @@ int main() {
     int maxTemp, minTemp, maxDay, minDay, lowZero = 0;
     double avgTemp, sum = 0;
 
+    cout << "Массив: " << endl;
     for (int i = 0; i < N; i++) {
         temps[i] = rand() % 51 - 20;
-        sum += temps[i];
+        cout << "temps[" << i << "] = " << temps[i] << "\t";
+        if ((i + 1) % 5 == 0)
+            cout << endl;
     }
+
+    cout << endl;
 
     maxTemp = temps[0];
     minTemp = temps[0];
@@ -29,13 +34,14 @@ int main() {
             maxTemp = temps[i];
             maxDay = i;
         }
-        if (temps[i] < minTemp) {
+        else if (temps[i] < minTemp) {
             minTemp = temps[i];
             minDay = i;
         }
         if (temps[i] < 0) {
             lowZero++;
         }
+        sum += temps[i];
     }
 
     avgTemp = sum / N;
