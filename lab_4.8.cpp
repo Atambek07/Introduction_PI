1966c4bd88f08449f340447cbd9189d9275a651d
Atambek <117630074+Atambek07@users.noreply.github.com>
2025-11-04 13:45:15 +0600

    Update array size and random number generation
    Changed array size and modified random number generation logic.

--- lab_4.8.cpp
+++ lab_4.8.cpp
@@ -8,22 +8,23 @@ int main() {
     setlocale(LC_ALL, "RU");
     srand(time(NULL));
 
-    const int N = 20;
+    const int N = 10;
     int arr[N] = {}, countEven, countPairs;
     countEven = 0, countPairs = 0;
 
+    cout << "Массив: " << endl;
     for (int i = 0; i < N; i++) {
-        arr[i] = rand() % 51 - 25;
+        arr[i] = rand() % 3;
+        cout << "Arr[" << i << "] = " << arr[i] << "\t";
+        if ((i + 1) % 5 == 0)
+            cout << endl;
     }
+    cout << endl;
 
     for (int i = 0; i < N; i++) {
         if (i % 2 == 0 && arr[i] % 2 != 0) countEven++;
         if (i < N - 1 && arr[i] == arr[i + 1]) countPairs++;
     }
-
-    cout << "Массив: ";
-    for (int i = 0; i < N; i++) cout << arr[i] << " ";
-    cout << endl;
 
     cout << "Количество элементов с четными индексами и нечетных: " << countEven << endl;
     cout << "Количество пар одинаковых соседних элементов: " << countPairs << endl;
