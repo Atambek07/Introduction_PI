b701c7f10346130af767103405f2ffedb03e4ecd
Atambek <117630074+Atambek07@users.noreply.github.com>
2025-11-04 13:29:21 +0600

    Increase array size and improve output formatting

--- lab_4.2.cpp
+++ lab_4.2.cpp
@@ -9,18 +9,18 @@ int main() {
     setlocale(LC_ALL, "");
     srand(time(NULL));
 
-    const int N = 10;
+    const int N = 20;
     int arr[N] = {};
     int maxVal, minVal;
 
+    cout << "Исходный массив: " << endl;
     for (int i = 0; i < N; i++) {
         arr[i] = rand() % 51 - 25;
-    }
+        cout << "Arr[" << i << "] = " << arr[i] << "\t";
+        if ((i + 1) % 5 == 0)
+            cout << endl;
 
-	cout << "Исходный массив: ";
-    for (int i = 0; i < N; i++) {
-        cout << arr[i] << " ";
-	}
+    }
 
 	cout << endl;
 
@@ -29,10 +29,15 @@ int main() {
 
     for (int i = 1; i < N; i++) {
         if (arr[i] > maxVal) maxVal = arr[i];
-        if (arr[i] < minVal) minVal = arr[i];
+        else if (arr[i] < minVal) minVal = arr[i];
     }
 
     int diff = maxVal - minVal;
+    
+    cout << "Максимальное значение: " << maxVal << endl;
+    cout << "Минимальное значание: " << minVal << endl;
+    cout << "Рзность: " << diff << endl;
+    cout << endl;
 
     for (int i = 0; i < N; i++) {
         if (arr[i] % 5 == 0 && arr[i] % 3 != 0) {
@@ -40,9 +45,12 @@ int main() {
         }
     }
 
-    cout << "Результат: ";
+    cout << "Результат: " << endl;
     for (int i = 0; i < N; i++) {
-        cout << arr[i] << " ";
+        cout << "Arr[" << i << "] = " << arr[i] << "\t";
+        if ((i + 1) % 5 == 0)
+            cout << endl;
+
     }
     cout << endl;
 
